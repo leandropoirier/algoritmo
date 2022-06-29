fb28a2d111706c6f352fba6c12329fdefd9c2e5d
leandropoirier <leandropoirier2@outlook.com>
2022-06-29 01:15:04 -0300

    desafio complementario

---
+++ script.js
@@ -0,0 +1,8 @@
+let i = parseInt(prompt("ingresar un numero menor a 30 para sumar de a 3"));
+do{
+    i = i + 3;
+    alert("la suma da "+i+" y si le sumo 3 mas")
+}
+while(i<20){
+    alert("HASTA AQUI LLEGAMOS REY")
+}
